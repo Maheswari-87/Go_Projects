b29bb441e155af90a979b42a871233e29b87080b
maheswarikoppoju <maheswari.koppoju@srsconsultinginc.com>
2021-05-03 12:10:04 +0530

    commit

--- CovidCountryState/covid5.go
+++ CovidCountryState/covid5.go
@@ -21,6 +21,12 @@ type CountryState struct {
 	Recovered string
 	Deaths    string
 }
+type StateData struct {
+	State     string
+	Confirmed string
+	Recovered string
+	Deaths    string
+}
 
 func CountryStateCovid(w http.ResponseWriter, r *http.Request) {
 	response, err := http.Get("https://covid-api.mmediagroup.fr/v1/cases")
@@ -45,7 +51,7 @@ func CountryStateCovid(w http.ResponseWriter, r *http.Request) {
 	v.GetObject().Visit(func(key []byte, values *fastjson.Value) {
 		keys = append(keys, string(key))
 	})
-	fmt.Println(keys)
+	//fmt.Println(keys)
 	var State string
 	var Confirmed float64
 	var Recovered float64
@@ -64,6 +70,7 @@ func CountryStateCovid(w http.ResponseWriter, r *http.Request) {
 		for k, v := range all {
 			if k == "All" {
 				allV := v.(map[string]interface{})
+
 				for key, value := range allV {
 					if key == "capital_city" && value != nil {
 						State = value.(string)
@@ -80,7 +87,7 @@ func CountryStateCovid(w http.ResponseWriter, r *http.Request) {
 				}
 			}
 		}
-		fmt.Println(State)
+		//fmt.Println(State)
 		s := strconv.FormatFloat(Confirmed, 'f', -1, 64)
 		t := strconv.FormatFloat(Recovered, 'f', -1, 64)
 		u := strconv.FormatFloat(Deaths, 'f', -1, 64)
@@ -109,6 +116,94 @@ func CountryStateCovid(w http.ResponseWriter, r *http.Request) {
 		p1.Execute(w, data1)
 	}
 }
+func individualStates(w http.ResponseWriter, r *http.Request) {
+	//CountryUrl:=map[string]interface{}{}
+	country := r.URL.Query().Get("country")
+	//path:="https://covid-api.mmediagroup.fr/v1/cases?country=?
+	//param := url.QueryEscape(country)
+	//fmt.Println(country)
+	//path := fmt.Sprintf("https://covid-api.mmediagroup.fr/v1/cases?country=?%s", param)
+	response, err := http.Get("https://covid-api.mmediagroup.fr/v1/cases?country=?")
+	if err != nil {
+		fmt.Printf("the http request got failed with error %s\n", err)
+	}
+	defer response.Body.Close()
+	data, _ := (ioutil.ReadAll(response.Body))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	responseObject := map[string]interface{}{}
+	json.Unmarshal(data, &responseObject)
+	stringdata := string(data)
+	var p fastjson.Parser
+	v, err := p.Parse(stringdata)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var keys []string
+	v.GetObject().Visit(func(key []byte, values *fastjson.Value) {
+		keys = append(keys, string(key))
+	})
+	//fmt.Println(keys)
+	var State string
+	var Confirmed float64
+	var Recovered float64
+	var Deaths float64
+	p1, err := template.ParseFiles("html/header_State.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	p1.Execute(w, "Hi")
+
+	for _, i := range keys {
+
+		all := responseObject[i].(map[string]interface{})
+		if i != "All" {
+			for key, value := range all {
+
+				if key == "confirmed" && value != nil {
+					Confirmed = value.(float64)
+				}
+				if key == "recovered" && value != nil {
+					Recovered = value.(float64)
+				}
+				if key == "deaths" && value != nil {
+					Deaths = value.(float64)
+				}
+			}
+		}
+
+		//fmt.Println(State)
+		s := strconv.FormatFloat(Confirmed, 'f', -1, 64)
+		t := strconv.FormatFloat(Recovered, 'f', -1, 64)
+		u := strconv.FormatFloat(Deaths, 'f', -1, 64)
+
+		data := []string{i, s, t, u}
+		file := ("C:\\Users\\SRS\\gocode\\src\\workspace\\CovidApp4\\data\\CountryStatedata.csv")
+		f, err := os.OpenFile(file, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+		if err != nil {
+			fmt.Println("Error: ", err)
+			return
+		}
+		m := csv.NewWriter(f)
+		m.Write(data)
+		m.Flush()
+
+		err = m.Error()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		p1, err := template.ParseFiles("html/stateWise.html")
+		data1 := StateData{State, s, t, u}
+		if err != nil {
+			panic(err)
+		}
+		p1.Execute(w, data1)
+	}
+}
+
 func home(w http.ResponseWriter, r *http.Request) {
 	homePage, err := template.ParseFiles("html/new.html")
 	if err != nil {
@@ -119,7 +214,8 @@ func home(w http.ResponseWriter, r *http.Request) {
 func handleRequests() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/1", CountryStateCovid)
-	log.Fatal(http.ListenAndServe(":7067", nil))
+	http.HandleFunc("/2", individualStates)
+	log.Fatal(http.ListenAndServe(":6003", nil))
 }
 func main() {
 	handleRequests()
